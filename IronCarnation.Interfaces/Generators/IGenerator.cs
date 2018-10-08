7ba19fc22a6ae1f68a00164ca51b1891e6c47f2f
Michael D. Kill <michael.kill@samaritanconsulting.com mailto:michael.kill@samaritanconsulting.com>
2018-10-07 22:27:58 -0500

    Inital Project Structure

---
+++ IronCarnation.Interfaces/Generators/IGenerator.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace IronCarnation.Interfaces.Generators
+{
+    public interface IGenerator
+    {
+        
+    }
+}
---
+++ IronCarnation.TestConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using IronCarnation.ModelConfig;
+
+namespace IronCarnation.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
